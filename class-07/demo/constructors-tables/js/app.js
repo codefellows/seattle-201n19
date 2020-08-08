9029a8e78ec8ea2c465db6697e542f973da787d2
Ryan Gallaway <rkgallaway@msn.com>
2020-08-08 12:35:44 -0700

    class7 demo added

--- class-07/demo/constructors-tables/js/app.js
+++ class-07/demo/constructors-tables/js/app.js
@@ -1,129 +1,136 @@
-// problem domain: the Seattle Kitten Rescue has tons of kittens who need good homes. One of the best ways to reach prospective adoptive homes is to have profiles for each kitten available on a website. There are hundreds of kittens, though, and only a few volunteers; it's too time-consuming to hand-code each kitten's profile on their website. They need a better way.
+// var car = {
+//    make: 'vw',
+//    model: 'gti',
+//    year: '2016',
+//    carMethod: function (){
+//      return 'This is a car, vroom vroom';
+//    }
+//  }
 
-// Each kitten's profile should have:
-// - name
-// - random age assigned
-// - a list of what they like
-// - an image
-// good with kids
-// good with dogs
-// good with other cats
-// breed
+//  console.log(car);
 
-// TODO: dynamically generate kitten objects using form data
+//  console.log(car.carMethod());
 
-'use strict';
 
-function Cat (breed, eyeColor) {
-  this.breed = breed;
-  this.eyeColor = eyeColor;
-}
-
-Cat.prototype.chaseLaserPointer = function () {
-  console.log('run around!!!!!!');
-};
-
-// create a kitten factory
-function Kitten (name, interests, isGoodWithKids, isGoodWithDogs, isGoodWithOtherCats) {
-  // this is a capital K kitten because it's a constructor function
-  this.name = name;
-  this.age = this.getAge();
-  this.interests = interests;
-  this.isGoodWithKids = isGoodWithKids;
-  this.isGoodWithDogs = isGoodWithDogs;
-  this.isGoodWithOtherCats = isGoodWithOtherCats;
-}
-
-// tell Kitten to inherit from Cat
-Kitten.prototype = new Cat();
-
-// add getAge method - here we define the function inline
-Kitten.prototype.getAge = function() {
-  this.age = randomAge(3, 12) + ' months';
-};
-
-Kitten.prototype.meow = function () {
-  console.log('meow!');
-};
-
-// add render method - here we define the function later
-Kitten.prototype.render = render;
-
-// tell the factory to create a specific kitten, using the new keyword
-var frankie = new Kitten('frankie', ['cuddling', 'chasing string', 'catnip'], true, false, true);
-frankie.getAge();
-frankie.meow();
-frankie.render();
-
-var serena = new Kitten('serena', ['sitting on laps', 'climbing curtains', 'eating treats'], true, null, true);
-serena.getAge();
-serena.render();
-
-var jumper = new Kitten('jumper', ['sunbeams', 'yarn', 'milk', 'paper bags'], false, true, true);
-jumper.getAge();
-jumper.eyeColor = 'green';
-jumper.render();
-
-function randomAge (min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function render () {
-  var parentElement = document.getElementById('kittenProfiles');
-
-  var article = document.createElement('article');
-  parentElement.appendChild(article);
-
-  var h2 = document.createElement('h2');
-  h2.textContent = this.name;
-  article.appendChild(h2);
-
-  var p = document.createElement('p');
-  p.textContent = this.name + ' is adorable, and is ' + this.age + ' old.';
-  article.appendChild(p);
-
-  var ul = document.createElement('ul');
-  article.appendChild(ul);
-
-  for (var i = 0; i < this.interests.length; i++) {
-    var li = document.createElement('li');
-    li.textContent = this.interests[i];
-    ul.appendChild(li);
+var deannaLiteral = {
+  isStudent: true,
+  name: 'Deanna',
+  hairColor: 'brown',
+  eyeColor: 'brown',
+  rating:  '5 star',
+  greeting: function(){
+    return `Hello all! I'm rated ${this.rating}`;
   }
-
-  var table = document.createElement('table');
-
-  var row1 = document.createElement('tr');
-  var row2 = document.createElement('tr');
-
-  var th1 = document.createElement('th');
-  th1.textContent = 'Good With Kids';
-  var th2 = document.createElement('th');
-  th2.textContent = 'Good With Dogs';
-  var th3 = document.createElement('th');
-  th3.textContent = 'Good With Other Cats';
-
-  var td1 = document.createElement('td');
-  td1.textContent = this.isGoodWithKids;
-  var td2 = document.createElement('td');
-  td2.textContent = this.isGoodWithDogs;
-  var td3 = document.createElement('td');
-  td3.textContent = this.isGoodWithOtherCats;
-
-  row1.appendChild(th1);
-  row1.appendChild(th2);
-  row1.appendChild(th3);
-
-  row2.appendChild(td1);
-  row2.appendChild(td2);
-  row2.appendChild(td3);
-
-  table.appendChild(row1);
-  table.appendChild(row2);
-  article.appendChild(table);
-
-  var img = document.createElement('img');
-  img.setAttribute('src', 'images/' + this.name + '.jpeg');
-  img.setAttribute('alt', 'cute picture of ' + this.name + ', who is an orange and white cat. You should really adopt him.');
-  article.appendChild(img);
 }
+
+var nick = {
+  student: true,
+  hairColor: 'brown',
+  eyeColor: 'blue',
+  rating: '5 star',
+  greeting: function(){
+    return `Hello all! I'm rated ${this.rating}`;
+  }
+}
+
+var example = nick.greeting();
+
+//  console.log(example);
+
+function Student (name, hair, eye, rating){
+ this.isStudent = true;
+ this.name = name;
+ this.hairColor = hair;
+ this.eyeColor = eye;
+ this.rating = rating;
+}
+
+Student.prototype.greeting = function(){
+  return `Hello all, my name is ${this.name}! I'm rated ${this.rating}`;
+}
+
+Student.prototype.render = function(){
+  //logic here
+}
+
+
+var deanna = new Student('Deanna', 'brown', 'brown', '5 star');
+var brandon = new Student('Brandon', 'brown', 'brown', '5 star');
+var henry = new Student('Henry', 'blond', 'blue', '5 star');
+
+// console.log(deannaLiteral);
+// console.log(deanna);
+// console.log(brandon);
+// console.log(henry);
+
+
+deanna.rating = '6 star';
+console.log('deanna\'s rating: ', deanna.rating);
+var anotherExample = deanna.greeting();
+
+console.log(anotherExample);
+console.log('----------------------------')
+
+deannaLiteral.isStudent = false;
+deannaLiteral.hasGraduate = true;
+console.log('deannaLiteral below:' );
+console.log(deannaLiteral);
+console.log('----------------------------')
+
+
+// Wednesday - Deanna Graduates
+deanna.isFabulous = true;
+deanna.isStudent = false;
+console.log(deanna);
+
+var parentEl = document.getElementById('students');
+
+//create new element (row)
+var trElement = document.createElement('tr');
+
+//give row its content
+var thElement = document.createElement('th');
+thElement.textContent = 'Name';
+trElement.appendChild(thElement);
+
+thElement = document.createElement('th');
+thElement.textContent = 'Hair Color';
+trElement.appendChild(thElement);
+
+thElement = document.createElement('th');
+thElement.textContent = 'Eye Color';
+trElement.appendChild(thElement);
+
+thElement = document.createElement('th');
+thElement.textContent = 'Rating';
+trElement.appendChild(thElement);
+
+//append row to parent
+parentEl.appendChild(trElement);
+
+
+var allStudents = [deanna, brandon, henry];
+
+for (var i = 0; i < allStudents.length; i++){
+  trElement = document.createElement('tr');
+  
+  var tdElement = document.createElement('td');
+  tdElement.textContent = allStudents[i].name;
+  trElement.appendChild(tdElement);
+  
+  tdElement = document.createElement('td');
+  tdElement.textContent = allStudents[i].hairColor;
+  trElement.appendChild(tdElement);
+  
+  tdElement = document.createElement('td');
+  tdElement.textContent = allStudents[i].eyeColor;
+  trElement.appendChild(tdElement);
+  
+  tdElement = document.createElement('td');
+  tdElement.textContent = allStudents[i].rating;
+  trElement.appendChild(tdElement);
+  
+  parentEl.appendChild(trElement);
+}  
+
+
